137c91a6b15db82cddf684bb401e3186665fbeb9
Jideofor <njusticej@gmail.com>
2020-05-14 20:37:55 +0100

    feat: add semantic css

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
+import 'semantic-ui-css/semantic.min.css'
 
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import Login from './components/auth/Login';
