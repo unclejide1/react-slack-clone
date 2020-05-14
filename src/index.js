2fa2ad7693b747c0fb96483cce329df13cfe95d2
Jideofor <njusticej@gmail.com>
2020-05-14 22:05:00 +0100

    feat: set up on auth state change listener

--- src/index.js
+++ src/index.js
@@ -1,22 +1,39 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
-import 'semantic-ui-css/semantic.min.css'
+import 'semantic-ui-css/semantic.min.css';
+import firebase from './firebase';
 
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import {BrowserRouter as Router, Switch, Route, withRouter} from 'react-router-dom';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
 
-const Root = () => (
-    <Router>
+class Root extends Component{
+    componentDidMount(){
+        firebase.auth().onAuthStateChanged(user => {
+            if(user){
+                this.props.history.push('/');
+            }
+        })
+    }
+render() {
+return (
+    
         <Switch>
             <Route exact path="/" component ={App}/>
             <Route path="/login" component ={Login}/>
             <Route path="/register" component ={Register}/>
         </Switch>
-    </Router>
+    
 )
+ }
+}
 
-ReactDOM.render(<Root />, document.getElementById('root'));
+const RootwithAuth = withRouter(Root);
+
+ReactDOM.render(
+<Router>
+    <RootwithAuth />
+</Router>, document.getElementById('root'));
 registerServiceWorker();
