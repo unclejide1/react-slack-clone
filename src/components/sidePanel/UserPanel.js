06eb7b04a343aad8bfb4ab05ff65ef7586fb64d4
Jideofor <njusticej@gmail.com>
2020-05-15 14:29:08 +0100

    feat: display avatar on user panel

--- src/components/sidePanel/UserPanel.js
+++ src/components/sidePanel/UserPanel.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Grid, Header, Icon, Dropdown } from "semantic-ui-react";
+import { Grid, Header, Icon, Dropdown, Image } from "semantic-ui-react";
 import firebase from "../../firebase";
 
 class UserPanel extends Component {
@@ -34,6 +34,7 @@ class UserPanel extends Component {
     }
     
     render(){
+        const {user} = this.state
         return (
             <Grid style={{background: '#4c3c4c'}}>
                 <Grid.Column>
@@ -44,13 +45,17 @@ class UserPanel extends Component {
                             <Header.Content>Dev-Connect</Header.Content>
 
                         </Header>
-                    </Grid.Row>
+                    
                     {/* user dropdown */}
                     <Header style={{padding: '0.25em'}} as="h4" inverted>
                         <Dropdown trigger={
-                            <span>{this.state.user.displayName}</span>}
+                            <span>
+                                <Image src={user.photoURL} spaced="right" avatar/>
+                                {user.displayName}
+                                </span>}
                             options={this.dropdownOptions()}/>
                     </Header>
+                    </Grid.Row>
                 </Grid.Column>
             </Grid>
         )
