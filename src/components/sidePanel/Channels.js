7645fe6a73d56e27ae9aaf4d1824813fbce484eb
Jideofor <njusticej@gmail.com>
2020-05-15 19:27:58 +0100

    feat: add modal to add new channels

--- src/components/sidePanel/Channels.js
+++ src/components/sidePanel/Channels.js
@@ -1,23 +1,58 @@
 import React, { Component } from "react";
-import { Menu, Icon } from "semantic-ui-react";
+import { Menu, Icon, Modal, Form, Input, Button } from "semantic-ui-react";
 
 class Channels extends Component {
     state = {
-        channels: []
+        channels: [],
+        channelName: '',
+        channelDetails: '',
+        modal: false
     }
 
+    handleChange = (event) => {
+        this.setState({ [event.target.name]: event.target.value });
+    };
+
+    closeModal = () => this.setState({modal: false})
+
+    openModal = () => this.setState({modal: true})
+
+
     render(){
-        const { channels } = this.state;
+        const { channels, modal } = this.state;
         return(
+            <React.Fragment>
             <Menu.Menu style={{paddingBottom: "2em"}}>
                 <Menu.Item>
                     <span>
                         <Icon name='exchange'/> CHANNELS
                     </span>{"  "}
-                    ({channels.length}) <Icon name="add"/>
+                    ({channels.length}) <Icon name="add" onClick ={this.openModal}/>
                 </Menu.Item>
-
             </Menu.Menu>
+             {/* add channel modal */}
+            <Modal basic open={modal} onClose={this.closeModal}>
+                <Modal.Header>Add a Channnel</Modal.Header>
+                <Modal.Content>
+                    <Form>
+                        <Form.Field>
+                           <Input fluid label="Name of Channel" name="channelName" onChange={this.handleChange} />
+                        </Form.Field>
+                        <Form.Field>
+                           <Input fluid label="About the Channel" name="channelDetails" onChange={this.handleChange} />
+                        </Form.Field>
+                    </Form>
+                </Modal.Content>
+                <Modal.Actions>
+                    <Button color="green" inverted>
+                        <Icon name='checkmark'/> Add
+                    </Button>
+                    <Button color="red" inverted onClick={this.closeModal}>
+                        <Icon name='remove'/> Cancel
+                    </Button>
+                </Modal.Actions>
+            </Modal>
+            </React.Fragment>
         )
     }
 }
